65db22368911c98790d2d5dab65db3028835e5d9
thejan14 <jan.oberpichler@googlemail.com>
2023-12-11 17:46:14 +0100

    refactor day 11

--- day11/part2.cpp
+++ day11/part2.cpp
@@ -5,96 +5,97 @@
 #include <chrono>
 #include <format>
 #include <array>
-#include <map>
 #include <ranges>
-#include <set>
 
-using pos = std::pair<int, int>;
+/* thanks to the reddit solutions thread for inspirations on improving my solution, especially u/endl */
+
+using pos = std::pair<long long, long long>;
 
 constexpr int DIM = 140;
 
 // https://stackoverflow.com/questions/1903954/is-there-a-standard-sign-function-signum-sgn-in-c-c
 template <typename T> int sgn(T val) {
-	return (T(0) < val) - (val < T(0));
+    return (T(0) < val) - (val < T(0));
 }
 
-long long getDistance(std::array<bool, DIM> const& galaxyRows, std::array<bool, DIM> const& galaxyColumns, pos const& galaxy1, pos  const& galaxy2)
+long long getDistance(pos const& galaxy1, pos  const& galaxy2)
 {
-	auto distance = 0LL;
-	auto const [gi1, gj1] = galaxy1;
-	auto const [gi2, gj2] = galaxy2;
-
-	auto const rowSign = sgn(gi2 - gi1);
-	for (auto i = gi1; i != gi2; i += rowSign)
-	{
-		distance += galaxyRows[i] ? 1 : 1'000'000;
-	}
-
-	auto const columnSign = sgn(gj2 - gj1);
-	for (auto j = gj1; j != gj2; j += columnSign)
-	{
-		distance += galaxyColumns[j] ? 1 : 1'000'000;
-	}
-
-	return distance;
+    return std::abs(galaxy1.first - galaxy2.first) + std::abs(galaxy1.second - galaxy2.second);
 }
 
 int main()
 {
-	std::string input;
-	if (auto readStream = std::ifstream("input.txt"); readStream.is_open())
-	{
-		std::string line;
-		while (getline(readStream, line))
-		{
-			input += line;
-			input.push_back('\n');
-		}
-	}
+    std::string input;
+    if (auto readStream = std::ifstream("input.txt"); readStream.is_open())
+    {
+        std::string line;
+        while (getline(readStream, line))
+        {
+            input += line;
+            input.push_back('\n');
+        }
+    }
 
-	const auto execStart = std::chrono::high_resolution_clock::now();
+    const auto execStart = std::chrono::high_resolution_clock::now();
 
-	/* begin solution */
+    /* begin solution */
 
-	auto galaxies = std::map<int, pos>{};
-	auto galaxyRows = std::array<bool, DIM>{};
-	auto galaxyColumns = std::array<bool, DIM>{};
-	auto g = 0; // running galaxy index
-	for (auto i = 0; i < DIM; i++)
-	{
-		for (auto j = 0; j < DIM; j++)
-		{
-			auto const c = input[i * (DIM + 1) + j];
-			if (c == '#')
-			{
-				galaxies[g++] = { i, j };
-				galaxyRows[i] = true;
-				galaxyColumns[j] = true;
-			}
-		}
-	}
+    // save distance added through expansion for each row/column, i.e.
+    // 0 for no expansion (contains galaxy) and 999'999 for expansion (999'999 more row/column)
+    auto expansionRows = std::array<long long, DIM>{};
+    auto expansionColumns = std::array<long long, DIM>{};
+    std::ranges::fill(expansionRows, 999'999);
+    std::ranges::fill(expansionColumns, 999'999);
 
-	auto pairs = std::set<std::pair<int, int>>{};
-	for (auto const id1 : galaxies | std::views::keys)
-	{
-		for (auto const id2 : galaxies | std::views::keys)
-		{
-			if (id1 != id2)
-			{
-				pairs.emplace(std::min(id1, id2), std::max(id1, id2));
-			}
-		}
-	}
+    auto galaxies = std::vector<pos>{};
+    for (auto i = 0; i < DIM; i++)
+    {
+        for (auto j = 0; j < DIM; j++)
+        {
+            auto const c = input[i * (DIM + 1) + j];
+            if (c == '#')
+            {
+                galaxies.emplace_back(i, j);
+                expansionRows[i] = 0;
+                expansionColumns[j] = 0;
+            }
+        }
+    }
 
-	auto answer = 0LL;
-	for (auto const [galaxy1, galaxy2] : pairs)
-	{
-		answer += getDistance(galaxyRows, galaxyColumns, galaxies[galaxy1], galaxies[galaxy2]);
-	}
+    // accumulate the expansion for each row/column, e.g:
+    // [0, 1, 1, 0, 0, 0, 1, 0] =>
+    // [0, 1, 2, 2, 2, 2, 3, 3]
+    for (auto i = 1; i < DIM; i++)
+    {
+        expansionRows[i] += expansionRows[i - 1];
+    }
 
-	/* end solution */
+    for (auto j = 1; j < DIM; j++)
+    {
+        expansionColumns[j] += expansionColumns[j - 1];
+    }
 
-	const auto execEnd = std::chrono::high_resolution_clock::now();
-	const auto micros = std::chrono::duration_cast<std::chrono::microseconds> (execEnd - execStart);
-	std::cout << std::format("{} ({})", answer, micros);
+    // use the expansion arrays to adjust the galaxy coordinates accordingly
+    // (i.e. add the accumulated expansion distance to the coordinates)
+    for (auto& [gi, gj] : galaxies)
+    {
+        gi += expansionRows[gi];
+        gj += expansionColumns[gj];
+    }
+
+    // calculate the manhattan distance between each unique pair of galaxies
+    auto answer = 0LL;
+    for (size_t i = 0; i < galaxies.size() - 1; i++)
+    {
+        for (size_t j = i + 1; j < galaxies.size(); j++)
+        {
+            answer += getDistance(galaxies[i], galaxies[j]);
+        }
+    }
+
+    /* end solution */
+
+    const auto execEnd = std::chrono::high_resolution_clock::now();
+    const auto micros = std::chrono::duration_cast<std::chrono::microseconds> (execEnd - execStart);
+    std::cout << std::format("{} ({})", answer, micros);
 }
