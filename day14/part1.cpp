f89efa31461d2388044e89d27758a758bed2348e
thejan14 <jan.oberpichler@googlemail.com>
2023-12-14 16:30:36 +0100

    solution day 14

---
+++ day14/part1.cpp
@@ -0,0 +1,86 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <chrono>
+#include <format>
+#include <ranges>
+
+template <typename T>
+auto split(T const& str, char sep)
+{
+    return str
+        | std::views::split(sep)
+        | std::views::transform([](auto const t) { return std::string_view(t); });
+}
+
+std::string getColumn(std::vector<std::string>& rows, size_t const column)
+{
+    return rows
+        | std::views::transform([column](auto line) { return line[column]; })
+        | std::ranges::to<std::string>();
+}
+
+int main()
+{
+    std::string input;
+    if (auto readStream = std::ifstream("input.txt"); readStream.is_open())
+    {
+        std::string line;
+        while (getline(readStream, line))
+        {
+            input += line;
+            input.push_back('\n');
+        }
+    }
+
+    const auto execStart = std::chrono::high_resolution_clock::now();
+
+    /* begin solution */
+
+    auto const sanitized = input.substr(0, input.size() - 1);
+    auto rows = split(sanitized, '\n')
+        | std::views::transform([](auto const row) { return std::string(row); })
+        | std::ranges::to<std::vector>();
+    auto columns = std::views::iota(0, static_cast<int>(rows.size()))
+        | std::views::transform([&rows](auto const column) { return getColumn(rows, column); })
+        | std::ranges::to<std::vector>();
+
+    auto answer = 0ULL;
+    for (auto& column : columns)
+    {
+        for (size_t i = 0; i < column.length(); i++)
+        {
+            if (column[i] == '.' && i < column.length() - 1)
+            {
+                auto j = i + 1;
+                auto current = column[j];
+                while (current == '.' && j < column.length())
+                {
+                    current = column[++j];
+                }
+
+                if (current == 'O')
+                {
+                    column[i] = 'O';
+                    column[j] = '.';
+                }
+
+                if (current == '#')
+                {
+                    i = j;
+                }
+            }
+
+            if (column[i] == 'O')
+            {
+                answer += column.length() - i;
+            }
+        }
+    }
+
+    /* end solution */
+
+    const auto execEnd = std::chrono::high_resolution_clock::now();
+    const auto micros = std::chrono::duration_cast<std::chrono::microseconds> (execEnd - execStart);
+    std::cout << std::format("{} ({})", answer, micros);
+}
---
+++ day14/part2.cpp
@@ -0,0 +1,153 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <chrono>
+#include <format>
+#include <ranges>
+#include <map>
+
+constexpr auto CYCLES = 1'000'000'000ULL;
+
+std::vector<std::vector<char*>> split(std::string& str, char sep)
+{
+    auto result = std::vector<std::vector<char*>>{};
+    auto current = std::vector<char*>{};
+    for (char& i : str)
+    {
+        if (i == '\n')
+        {
+            result.push_back(current);
+            current.clear();
+        }
+        else
+        {
+            current.push_back(&i);
+        }
+    }
+
+    result.push_back(current);
+    return result;
+}
+
+std::vector<char*> getColumn(std::vector<std::vector<char*>>& rows, size_t const column)
+{
+    return rows
+        | std::views::transform([column](auto& line) { return line[column]; })
+        | std::ranges::to<std::vector>();
+}
+
+void tilt(std::vector<std::vector<char*>> const& view)
+{
+    for (auto& ray : view)
+    {
+        for (size_t i = 0; i < ray.size(); i++)
+        {
+            if (*ray[i] == '.' && i < ray.size() - 1)
+            {
+                auto j = i + 1;
+                auto next = *ray[j];
+                while (next == '.' && j < ray.size() - 1)
+                {
+                    next = *ray[++j];
+                }
+
+                if (next == 'O')
+                {
+                    *ray[i] = 'O';
+                    *ray[j] = '.';
+                }
+
+                if (next == '#')
+                {
+                    i = j;
+                }
+            }
+        }
+    }
+}
+
+size_t getLoad(std::vector<std::vector<char*>> const& view)
+{
+    auto load = 0ULL;
+    for (auto& ray : view)
+    {
+        for (size_t i = 0; i < ray.size(); i++)
+        {
+            if (*ray[i] == 'O')
+            {
+                load += ray.size() - i;
+            }
+        }
+    }
+
+    return load;
+}
+
+int main()
+{
+    std::string input;
+    if (auto readStream = std::ifstream("input.txt"); readStream.is_open())
+    {
+        std::string line;
+        while (getline(readStream, line))
+        {
+            input += line;
+            input.push_back('\n');
+        }
+    }
+
+    const auto execStart = std::chrono::high_resolution_clock::now();
+
+    /* begin solution */
+
+    auto state = input.substr(0, input.size() - 1);
+
+    // rows from left to right
+    auto westView = split(state, '\n');
+
+    // rows from right to left
+    auto eastView = westView
+        | std::views::transform([](auto& row) { return row | std::views::reverse | std::ranges::to<std::vector>(); })
+        | std::ranges::to<std::vector>();
+
+    // columns from top to bottom
+    auto northView = std::views::iota(0, static_cast<int>(westView.size()))
+        | std::views::transform([&westView](auto const column) { return getColumn(westView, column); })
+        | std::ranges::to<std::vector>();
+
+    // columns from bottom to top
+    auto southView = northView
+        | std::views::transform([](auto& column) { return column | std::views::reverse | std::ranges::to<std::vector>(); })
+        | std::ranges::to<std::vector>();
+
+    auto states = std::map<std::string, size_t>{};
+    for (auto i = 0ULL; i < CYCLES; i++)
+    {
+        tilt(northView);
+        tilt(westView);
+        tilt(southView);
+        tilt(eastView);
+        if (auto const it = states.find(state); it == states.end())
+        {
+            states.emplace(state, i);
+        }
+        else
+        {
+            auto const& [_, j] = *it;
+            auto const cycleLength = i - j;
+            if (auto const remaining = CYCLES - i; remaining > cycleLength)
+            {
+                auto skip = remaining / cycleLength;
+                i += cycleLength * skip;
+            }
+        }
+    }
+
+    auto const answer = getLoad(northView);
+
+    /* end solution */
+
+    const auto execEnd = std::chrono::high_resolution_clock::now();
+    const auto micros = std::chrono::duration_cast<std::chrono::microseconds> (execEnd - execStart);
+    std::cout << std::format("{} ({})", answer, micros);
+}
