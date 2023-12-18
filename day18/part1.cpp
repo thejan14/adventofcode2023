03f8d176455602489d5116a29a090e3613f7993e
thejan14 <jan.oberpichler@googlemail.com>
2023-12-18 16:46:22 +0100

    solution day 18

---
+++ day18/part1.cpp
@@ -0,0 +1,136 @@
+﻿#include <iostream>
+#include <string>
+#include <chrono>
+#include <format>
+#include <ranges>
+#include <map>
+#include <set>
+#include <queue>
+
+#include "aoc.h"
+
+enum Direction { N, E, S, W };
+
+static int svtoi(std::string_view const& sv)
+{
+    int value;
+    if (std::from_chars(sv.data(), sv.data() + sv.size(), value).ec == std::errc{})
+    {
+        return value;
+    }
+
+    throw std::invalid_argument("not a number");
+}
+
+template <typename T>
+auto split(T const& str, char sep)
+{
+    return str
+        | std::views::split(sep)
+        | std::views::transform([](auto const t) { return std::string_view(t); });
+}
+
+Direction getDirection(char c)
+{
+    switch (c)
+    {
+        case 'U': return N;
+        case 'R': return E;
+        case 'D': return S;
+        case 'L': return W;
+        default: throw std::invalid_argument("direction does not exist");
+    }
+}
+
+std::pair<int, int> moveToDirection(std::pair<int, int> current, Direction const d)
+{
+    auto const [i, j] = current;
+    switch (d)
+    {
+        case N: return std::make_pair(i - 1, j);
+        case E: return std::make_pair(i, j + 1);
+        case S: return std::make_pair(i + 1, j);
+        case W: return std::make_pair(i, j - 1);
+    }
+}
+
+char getWinding(Direction const d)
+{
+    switch (d)
+    {
+        case N: return 'N';
+        case E: return 'X';
+        case S: return 'S';
+        case W: return 'X';
+    }
+}
+
+int main()
+{
+    auto input = aoc::readInput();
+    const auto execStart = std::chrono::high_resolution_clock::now();
+
+    /* begin solution */
+
+    auto outline = std::map<std::pair<int, int>, char>{};
+    auto current = std::make_pair(0, 0);
+    outline[current] = 'X';
+    auto minX = 0;
+    auto minY = 0;
+    auto maxX = 0;
+    auto maxY = 0;
+    for (auto const line : split(input, '\n'))
+    {
+        auto const plan = split(line, ' ') | std::ranges::to<std::vector>();
+        auto const direction = getDirection(plan[0][0]);
+        auto const distance = svtoi(plan[1]);
+        auto const winding = getWinding(direction);
+        if (winding == 'S')
+        {
+            outline[current] = 'S';
+        }
+
+        for (auto i = 0; i < distance; i++)
+        {
+            current = moveToDirection(current, direction);
+            outline[current] = winding;
+            minX = std::min(current.second, minX);
+            minY = std::min(current.first, minY);
+            maxX = std::max(current.second, maxX);
+            maxY = std::max(current.first, maxY);
+        }
+
+        if (winding == 'S')
+        {
+            outline[current] = 'X';
+        }
+    }
+
+    auto answer = outline.size();
+    for (auto i = minY; i <= maxY; i++)
+    {
+        auto windings = 0;
+        for (auto j = minX; j <= maxX; j++)
+        {
+            auto current = '.';
+            if (auto it = outline.find(std::make_pair(i, j)); it != outline.end())
+            {
+                auto const [_, w] = *it;
+                current = w;
+            }
+
+            switch (current)
+            {
+                case 'N': --windings; break;
+                case 'S': ++windings; break;
+                default: if (current != 'X' && windings != 0) ++answer;
+            }
+        }
+    }
+
+    /* end solution */
+
+    const auto execEnd = std::chrono::high_resolution_clock::now();
+    const auto micros = std::chrono::duration_cast<std::chrono::microseconds> (execEnd - execStart);
+    std::cout << std::format("{} ({})", answer, micros);
+}
---
+++ day18/part2.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+#include <string>
+#include <chrono>
+#include <format>
+#include <ranges>
+
+#include "aoc.h"
+
+enum Direction { N = 3, E = 0, S = 1, W = 2 };
+
+static long long hextoll(std::string_view const& sv)
+{
+    long long value;
+    if (std::from_chars(sv.data(), sv.data() + sv.size(), value, 16).ec == std::errc{})
+    {
+        return value;
+    }
+
+    throw std::invalid_argument("not a number");
+}
+
+template <typename T>
+auto split(T const& str, char sep)
+{
+    return str
+        | std::views::split(sep)
+        | std::views::transform([](auto const t) { return std::string_view(t); });
+}
+
+std::pair<Direction, long long> parseHexInstruction(std::string_view const& instruction)
+{
+    auto const distance = hextoll(instruction.substr(0, 5));
+    auto const direction = static_cast<Direction>(hextoll(instruction.substr(5)));
+    return std::make_pair(direction, distance);
+}
+
+std::pair<long long, long long> moveToDirection(std::pair<long long, long long> current, Direction const direction, long long const distance)
+{
+    auto const [i, j] = current;
+    switch (direction)
+    {
+        case N: return std::make_pair(i - distance, j);
+        case E: return std::make_pair(i, j + distance);
+        case S: return std::make_pair(i + distance, j);
+        case W: return std::make_pair(i, j - distance);
+    }
+}
+
+int main()
+{
+    auto input = aoc::readInput();
+    const auto execStart = std::chrono::high_resolution_clock::now();
+
+    /* begin solution */
+
+    auto area = 0LL;
+    auto outline = 0LL;
+    auto current = std::make_pair(0LL, 0LL);
+    for (auto const line : split(input, '\n'))
+    {
+        auto const hashPos = line.find('#');
+        auto const [direction, distance] = parseHexInstruction(line.substr(hashPos + 1, 6));
+        auto const next = moveToDirection(current, direction, distance);
+        auto const [ci, cj] = current;
+        auto const [ni, nj] = next;
+        area += (ci + ni) * (cj - nj); // https://en.wikipedia.org/wiki/Shoelace_formula
+        outline += distance;
+        current = next;
+    }
+
+    // thanks to u/echlos021 (https://www.reddit.com/r/adventofcode/comments/18l2tap/comment/kdv8imu) 
+    auto const answer = (area / 2) + (outline / 2) + 1;
+
+    /* end solution */
+
+    const auto execEnd = std::chrono::high_resolution_clock::now();
+    const auto micros = std::chrono::duration_cast<std::chrono::microseconds> (execEnd - execStart);
+    std::cout << std::format("{} ({})", answer, micros);
+}
