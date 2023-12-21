a5595b57c09843875369dca7b088e4ce1c1993df
thejan14 <jan.oberpichler@googlemail.com>
2023-12-21 13:10:43 +0100

    solution day 19

---
+++ day19/part1.cpp
@@ -0,0 +1,153 @@
+﻿#include <iostream>
+#include <string>
+#include <chrono>
+#include <format>
+#include <ranges>
+#include <map>
+#include <functional>
+
+#include "aoc.h"
+
+struct Part
+{
+    int x, m, a, s;
+
+    int getRatingSum() const
+    {
+        return x + m + a + s;
+    }
+};
+
+struct Workflow
+{
+    std::vector<std::function<std::pair<bool, std::string>(Part const)>> rules;
+
+    std::string processPart(Part const part) const
+    {
+        for (auto const subRule : this->rules)
+        {
+            auto const [applies, target] = subRule(part);
+            if (applies)
+            {
+                return target;
+            }
+        }
+
+        throw std::domain_error("invalid rule");
+    }
+};
+
+static int svtoi(std::string_view const& sv)
+{
+    int value;
+    if (std::from_chars(sv.data(), sv.data() + sv.size(), value).ec == std::errc{})
+    {
+        return value;
+    }
+
+    throw std::invalid_argument("not a number");
+}
+
+template <typename T>
+auto split(T const& str, char sep)
+{
+    return str
+        | std::views::split(sep)
+        | std::views::transform([](auto const t) { return std::string_view(t); });
+}
+
+std::pair<std::string, Workflow> parseWorkflow(std::string_view line)
+{
+    auto workflow = Workflow{};
+    auto const bracketPos = line.find('{');
+    auto const name = std::string{ line.substr(0, bracketPos) };
+    auto const rules = line.substr(bracketPos + 1, line.size() - bracketPos - 2);
+    for (auto const rule : split(rules, ','))
+    {
+        if (auto const colonPos = rule.find(':'); colonPos != -1)
+        {
+            auto const rating = rule[0];
+            auto const limit = svtoi(rule.substr(2, colonPos));
+            auto const target = std::string{ rule.substr(colonPos + 1) };
+            std::function<bool(int const a, int const b)> const op = rule[1] == '<'
+                ? [](int const a, int const b) { return a < b; }
+                : [](int const a, int const b) { return a > b; };
+            switch (rating)
+            {
+                case 'x':
+                    workflow.rules.emplace_back([op, limit, target](Part const p) { return std::make_pair(op(p.x, limit), target); });
+                    break;
+                case 'm':
+                    workflow.rules.emplace_back([op, limit, target](Part const p) { return std::make_pair(op(p.m, limit), target); });
+                    break;
+                case 'a':
+                    workflow.rules.emplace_back([op, limit, target](Part const p) { return std::make_pair(op(p.a, limit), target); });
+                    break;
+                case 's':
+                    workflow.rules.emplace_back([op, limit, target](Part const p) { return std::make_pair(op(p.s, limit), target); });
+                    break;
+                default:
+                    throw std::invalid_argument("rating category does not exist");
+            }
+        }
+        else
+        {
+            auto const target = std::string{ rule };
+            workflow.rules.emplace_back([target](Part const p) { return std::make_pair(true, target); });
+        }
+    }
+
+    return std::make_pair(name, workflow);
+}
+
+Part parsePart(std::string_view const line)
+{
+    auto const ratings = split(line.substr(1, line.size() - 2), ',')
+        | std::views::transform([](auto const r) { return svtoi(r.substr(2)); })
+        | std::ranges::to<std::vector>();
+    return {
+        ratings[0],
+        ratings[1],
+        ratings[2],
+        ratings[3],
+    };
+}
+
+int main()
+{
+    auto input = aoc::readInput();
+    const auto execStart = std::chrono::high_resolution_clock::now();
+
+    /* begin solution */
+
+    auto const sepPos = input.find("\n\n");
+    auto const rulesInput = input.substr(0, sepPos);
+    auto const partsInput = input.substr(sepPos + 2);
+    auto workflows = std::map<std::string, Workflow>();
+    for (auto const line : split(rulesInput, '\n'))
+    {
+        workflows.insert(parseWorkflow(line));
+    }
+
+    auto answer = 0;
+    for (auto const line : split(partsInput, '\n'))
+    {
+        auto const part = parsePart(line);
+        auto currentWorkflow = std::string{ "in" };
+        while (currentWorkflow != "A" && currentWorkflow != "R")
+        {
+            currentWorkflow = workflows.at(currentWorkflow).processPart(part);
+        }
+
+        if (currentWorkflow == "A")
+        {
+            answer += part.getRatingSum();
+        }
+    }
+
+    /* end solution */
+
+    const auto execEnd = std::chrono::high_resolution_clock::now();
+    const auto micros = std::chrono::duration_cast<std::chrono::microseconds> (execEnd - execStart);
+    std::cout << std::format("{} ({})", answer, micros);
+}
---
+++ day19/part2.cpp
@@ -0,0 +1,136 @@
+﻿#include <iostream>
+#include <string>
+#include <chrono>
+#include <format>
+#include <ranges>
+#include <map>
+#include <algorithm>
+
+#include "aoc.h"
+
+struct Rule
+{
+    char rating;
+    char op;
+    int limit;
+    std::string target;
+};
+
+static int svtoi(std::string_view const& sv)
+{
+    int value;
+    if (std::from_chars(sv.data(), sv.data() + sv.size(), value).ec == std::errc{})
+    {
+        return value;
+    }
+
+    throw std::invalid_argument("not a number");
+}
+
+template <typename T>
+auto split(T const& str, char sep)
+{
+    return str
+        | std::views::split(sep)
+        | std::views::transform([](auto const t) { return std::string_view(t); });
+}
+
+std::pair<std::string, std::vector<Rule>> parseWorkflow(std::string_view line)
+{
+    auto workflow = std::vector<Rule>{};
+    auto const bracketPos = line.find('{');
+    auto const name = std::string{ line.substr(0, bracketPos) };
+    auto const rules = line.substr(bracketPos + 1, line.size() - bracketPos - 2);
+    for (auto const rule : split(rules, ','))
+    {
+        if (auto const colonPos = rule.find(':'); colonPos != -1)
+        {
+            auto const rating = rule[0];
+            auto const op = rule[1];
+            auto const limit = svtoi(rule.substr(2, colonPos));
+            auto const target = std::string{ rule.substr(colonPos + 1) };
+            workflow.emplace_back(rating, op, limit, target);
+        }
+        else
+        {
+            auto const target = std::string{ rule };
+            workflow.emplace_back('_', '_', 0, target);
+        }
+    }
+
+    return std::make_pair(name, workflow);
+}
+
+unsigned long long getAccepted(std::map<std::string, std::vector<Rule>> const& workflows, std::map<char, std::pair<int, int>>& ranges, std::string const current)
+{
+    if (current == "A")
+    {
+        return std::ranges::fold_left(ranges | std::views::values, 1ULL, [](auto acc, auto const range) { return acc * ((range.second - range.first) + 1); });
+    }
+    else if (current == "R")
+    {
+        return 0;
+    }
+    
+    auto accepted = 0ULL;
+    for (auto const& [rating, op, limit, target] : workflows.at(current))
+    {
+        if (rating == '_')
+        {
+            accepted += getAccepted(workflows, ranges, target);
+        }
+        else
+        {
+            auto const& [from, to] = ranges[rating];
+            if (from < limit && to > limit)
+            {
+                auto newRanges = ranges;
+                if (op == '<')
+                {
+                    newRanges[rating] = std::make_pair(from, limit - 1);
+                    ranges[rating] = std::make_pair(limit, to);
+                }
+                else
+                {
+                    newRanges[rating] = std::make_pair(limit + 1, to);
+                    ranges[rating] = std::make_pair(from, limit);
+                }
+
+                accepted += getAccepted(workflows, newRanges, target);
+            }
+        }
+    }
+
+    return accepted;
+}
+
+int main()
+{
+    auto input = aoc::readInput();
+    const auto execStart = std::chrono::high_resolution_clock::now();
+
+    /* begin solution */
+
+    auto const sepPos = input.find("\n\n");
+    auto const rulesInput = input.substr(0, sepPos);
+    auto workflows = std::map<std::string, std::vector<Rule>>();
+    for (auto const line : split(rulesInput, '\n'))
+    {
+        workflows.insert(parseWorkflow(line));
+    }
+
+    auto ranges = std::map<char, std::pair<int, int>>{
+        { 'x', { 1, 4000 } },
+        { 'm', { 1, 4000 } },
+        { 'a', { 1, 4000 } },
+        { 's', { 1, 4000 } },
+    };
+    
+    auto const answer = getAccepted(workflows, ranges, "in");
+
+    /* end solution */
+
+    const auto execEnd = std::chrono::high_resolution_clock::now();
+    const auto micros = std::chrono::duration_cast<std::chrono::microseconds> (execEnd - execStart);
+    std::cout << std::format("{} ({})", answer, micros);
+}
